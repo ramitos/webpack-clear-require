d2e1d3e5945f6be9c9221bddd2ce08c60b5e80d3
Sérgio Ramos <mail@sergioramos.me>
2015-11-30 23:02:18 +0000

    initial commit

---
+++ readme.md
@@ -0,0 +1,19 @@
+# webpack-clear-require
+
+## install
+
+```bash
+npm install --save webpack-clear-require
+```
+
+## example
+
+```js
+var clearRequire = require('webpack-clear-require');
+clearRequire('react');
+clearRequire(); // all
+```
+
+## license
+
+BSD-3-Clause
---
+++ src/webpack-clear-require.js
@@ -0,0 +1,20 @@
+var forEach = require('lodash.foreach');
+var isString = require('lodash.isstring');
+var keys = require('lodash.keys');
+
+
+var clear = function(id) {
+  delete cache[id];
+};
+
+var all = function() {
+  forEach(keys(require.cache), clear);
+};
+
+module.exports = function(name) {
+  if (!isString(name)) {
+    return all();
+  }
+
+  clear(require.resolve(name));
+};
